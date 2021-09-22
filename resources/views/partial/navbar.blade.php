102186ff7603965b4f726a49442a2f6e060292ad
Finndy Noverita <finndy.noverita.syafitri-2020@vokasi.unair.ac.id>
2021-09-22 14:34:54 +0700

    commit2

---
+++ resources/views/partial/navbar.blade.php
@@ -0,0 +1,26 @@
+<nav class="navbar navbar-expand-lg navbar-dark bg-primary">
+  <div class="container-fluid">
+  <a class="navbar-brand" href="#">
+    <img src="img/pelaporan_bencana.png"/ width="150"> 
+    </a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNav">
+      <ul class="navbar-nav">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="/home">HOME</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="/about">ABOUT</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="/news">NEWS</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="/login">LOGIN</a>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
