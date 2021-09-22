3316c54e8a8371d5fad494537fa3ba8183b3b06a
Finndy Noverita <finndy.noverita.syafitri-2020@vokasi.unair.ac.id>
2021-09-22 13:24:46 +0700

    commit2

--- routes/web.php
+++ routes/web.php
@@ -14,8 +14,16 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/home', function () {
-return view('home');
-});
+    return view('home');
+    });
+
+Route::get('/about', function () {
+    return view('about');
+    });
+
+Route::get('/news', function () {
+    return view('news');
+     });    
 
 
 Route::get('/login', function () {
