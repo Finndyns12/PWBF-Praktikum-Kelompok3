d3d6366b8b0984827c2eaa9062dcbca91caefb2e
Finndy Noverita <finndy.noverita.syafitri-2020@vokasi.unair.ac.id>
2021-09-22 15:58:32 +0700

    commit2

--- resources/views/news.blade.php
+++ resources/views/news.blade.php
@@ -1,5 +1,12 @@
 @extends('layout.main')
 
 @section('container')
-    <h3>News </h3>
+   
+@foreach ($news as $news)
+<h3>{{ $news["title"] }}</h3>
+<h5>{{ $news["time"] }}</h5>
+<h5>{{ $news["location"] }}</h5>
+<h5>{{ $news["kronologi"] }}</h5>
+@endforeach
+
 @endsection
