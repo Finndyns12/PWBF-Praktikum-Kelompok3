42ad873df444b83714685db85a3d7528cf1b0077
Finndy Noverita <finndy.noverita.syafitri-2020@vokasi.unair.ac.id>
2021-09-22 13:14:52 +0700

    commit2

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -8,13 +8,13 @@
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-F3w7mX95PdgyTmZZMECAngseQB83DfGTowi0iMjiWaeVhAn4FJkqJByhZMI3AhiU" crossorigin="anonymous">
 
-    <title>Sistem Informasi Pelaporan Bencana | Home</title>
+    <title>SIPB | Home</title>
   </head>
   <body>
   <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
   <div class="container-fluid">
   <a class="navbar-brand" href="#">
-    <img src="img/pelaporan_bencana.png"/ width="80"> 
+    <img src="img/pelaporan_bencana.png"/ width="150"> 
 </a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
