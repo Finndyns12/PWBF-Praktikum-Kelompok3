5c8639d30e6235277ab8fc10a834432eee8e4b38
Finndy Noverita <finndy.noverita.syafitri-2020@vokasi.unair.ac.id>
2021-10-21 10:56:30 +0700

    commit new

--- routes/web.php
+++ routes/web.php
@@ -11,24 +11,9 @@ Route::get('/home', function () {
 });
 
 Route::get('/news', function () {
-    
-        $news_post = [
-        [
-                "title" => "Bencana Alam Banjir,Longsor, &  Angin Puting Beliung",
-                "slug" => "bencana-alam-banjir-longsor-&-angin-puting-beliung",
-                "time" => "Senin 21 September 2020 Pukul 16.00 Wib",
-                "location" => "Kec. Pamijahan, Kec.Ciawi, Kec Cisarua, Kec.Caringin, Kec.Cijeruk"
-            ],
-         [
-                "title" => "Bencana Alam Angin Ribut",
-                "slug" => "bencana-alam-angin-ribut",
-                "time" => "28 Maret 2021 Pukul 14.00 WIB",
-                "location" => "Kecamatan Jaten dan Kecamatan Tasikmadu, Kabupaten Karanganyar"
-        ],
-    ];
     return view('news',[
         "title" => "News",
-        "news" => $news_post
+        "news" => 
     ]);
 });    
 
