5c2af0d1c4c7307c2f9115ab79efafd00f93b335
Finndy Noverita <finndy.noverita.syafitri-2020@vokasi.unair.ac.id>
2021-10-24 23:09:07 +0700

    commitnov

--- resources/views/partial/navbar.blade.php
+++ resources/views/partial/navbar.blade.php
@@ -1,8 +1,6 @@
 <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
   <div class="container-fluid">
-    <a class="navbar-brand" href="{{ url('/home')}}">
-    Pelaporan Bencana
-    </a>
+    <a class="navbar-brand" href="{{ url('/home')}}">Pelaporan Bencana</a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
@@ -17,8 +15,10 @@
         <li class="nav-item">
           <a class="nav-link {{ ($title === "News" ) ? 'active' : '' }}"  href="{{ url('/news')}}">NEWS</a>
         </li>
+      </ul>
+      <ul class="navbar-nav ms-auto">
         <li class="nav-item">
-          <a class="nav-link {{ ($title === "Login" ) ? 'active' : '' }}" href="{{ url('/login')}}">LOGIN</a>
+          <a href="/login" class="nav-link">Login</a>
         </li>
       </ul>
     </div>
