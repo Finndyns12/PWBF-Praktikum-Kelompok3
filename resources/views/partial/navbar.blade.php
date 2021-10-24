a57c5f0b4de1749de2989f9415e9d38c28f93259
Finndy Noverita <finndy.noverita.syafitri-2020@vokasi.unair.ac.id>
2021-10-24 23:22:51 +0700

    24

--- resources/views/partial/navbar.blade.php
+++ resources/views/partial/navbar.blade.php
@@ -15,9 +15,6 @@
         <li class="nav-item">
           <a class="nav-link {{ ($title === "News" ) ? 'active' : '' }}" href="/news">NEWS</a>
         </li>
-        <li class="nav-item">
-          <a class="nav-link {{ ($title === "Login" ) ? 'active' : '' }}" href="/login">LOGIN</a>
-        </li>
       </ul>
     </div>
   </div>
